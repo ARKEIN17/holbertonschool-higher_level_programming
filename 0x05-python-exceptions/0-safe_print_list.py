ad038ff295bfd4997ed577b45cc3d466b5070c1e
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-05-23 17:20:19 -0500

    git add

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,12 +4,12 @@
 def safe_print_list(my_list=[], x=0):
 
     i = 0
-    prt = 0
+    printed = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            prt += 1
+            printed += 1
         except TypeError:
             continue
     print()
-    return prt
+    return printed
