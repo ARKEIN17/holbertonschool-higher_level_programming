7a867ebf346c86343b7844d38671ec75dfa4e4c3
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-05-23 17:10:09 -0500

    typeerror

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 
-
 def safe_print_list(my_list=[], x=0):
+
     i = 0
     cont = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
             cont += 1
-        except:
+        except TypeError:
             continue
     print()
     return cont
