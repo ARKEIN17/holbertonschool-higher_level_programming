ff9c766805d1bb8b3a40f3aff31e22e91716c67a
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-05-23 20:40:53 -0500

    escepction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             cont += 1
-        except TypeError:
+        except Exception:
             continue
     print()
     return cont
