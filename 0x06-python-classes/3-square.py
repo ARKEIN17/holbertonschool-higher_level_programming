3abfa68e3b6c0f933f5cc70fd2f7c4cf1ca587b6
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-05-25 20:19:06 -0500

    area of squa

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,9 +12,10 @@ class Square:
     def __init__(self, size=0):
         '''Validations'''
         if type(size) != int:
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
+            '''Less than 0'''
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
 
         self.__size = size
 
