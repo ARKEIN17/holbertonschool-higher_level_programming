0f039fddfd884222031fa82992b9faf8fe013f27
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-05-25 21:37:00 -0500

    Create a empty class

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-
 """Create a empty class"""
 
 
 class Square:
+    """square with size"""
     def __init__(self, size=0):
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
+        """getting size atributte"""
         return self.__size
-    '''Definitios'''
+
     @size.setter
     def size(self, value):
-        '''Validations'''
-        if type(value) != int:
-            raise TypeError('size must be an integer')
+        """adding value to size atribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        a = self.__size * self.__size
-        '''Returns'''
-        return a
+        """area of the operation"""
+        return self.__size ** 2
