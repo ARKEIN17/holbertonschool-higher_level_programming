0b77e2552d0518a3320adadd5acf74f599f53faf
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-04 11:09:17 -0500

    changes

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""create a locked class"""
+
+
+class LockedClass:
+    """locked class with firt name like argument"""
+
+    __slots__ = 'first_name'
+
+    def __init__(self):
+        """init method"""
+    pass
