895ee32f8de973337d1d72a58b701951091757c0
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-08 17:15:57 -0500

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" funtion tath write in a file """
 
 
 def append_write(filename="", text=""):
+    """ open file appends a string in the end of file """
     with open(filename, "a", encoding="UTF-8") as f:
+        """ retur """
         return f.write(text)
