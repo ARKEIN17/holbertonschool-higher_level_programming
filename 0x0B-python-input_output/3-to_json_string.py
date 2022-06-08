a5f98378a388097ddf2a2fd471ffadd5a73bdb7a
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-08 17:31:09 -0500

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """ conver a object a  string """
 import json
-
-from yaml import _Representer
 """ import a lib json and def json """
 
 
 def to_json_string(my_obj):
-    """ my funtion """
-    Represent = json.dumps(my_obj)
-    return Represent
+    """ retunt json """
+    return json.dumps(my_obj)
