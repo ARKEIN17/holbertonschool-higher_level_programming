555f8d6dd58529fb47513d63f84602a76b02e346
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-08 17:45:34 -0500

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ funtion that save"""
-
 import json
 
-""" funtion adn definition """
 
 def save_to_json_file(my_obj, filename):
+    """ funtion adn definition """
     with open(filename, "w", encoding="UTF-8") as f:
         json.dump(my_obj, f)
