912971cb0d580f5c78c21315dab5589daf867ca3
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-08 20:00:32 -0500

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-
 """A script that adds all arguments to a Python list, and then save them
 to a file."""
 
 from sys import argv
 from os import path
-
+""" save flie 5 and load file 6 """
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+"""add item """
 filename = 'add_item.json'
 if path.isfile(filename):
     new_list = load_from_json_file(filename)
@@ -16,4 +16,5 @@ else:
     new_list = []
 for i in range(1, len(argv)):
     new_list.append(argv[i])
+    """ definition """
 save_to_json_file(new_list, filename)
