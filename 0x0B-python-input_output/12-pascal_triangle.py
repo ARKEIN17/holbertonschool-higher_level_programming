7bacbec0c98438652b3e9d43d1cf4af0fcc13da2
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-08 20:46:44 -0500

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,10 +3,10 @@
 
 
 def PascalTriangle(n):
-    """mode triangle"""
-   trow = [1]
-   y = [0]
+   trow = 1
+   y = 0
+   """Inside the for loop we will print the list initialized by trow variable"""
    for x in range(n):
       print(trow)
-      trow=[left+right for left,right in zip(trow+y, y+trow)]
-   return n <= 0
+      trow = [left + right for left, right in zip(trow + y, y + trow)]
+   return n >= 0
