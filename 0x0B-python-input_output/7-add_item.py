4c113d286a17177c849c78163ffdbd4866d16c75
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-08 19:08:34 -0500

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,18 +6,21 @@ import sys
 from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""save"""
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-"""coment"""
+"""load"""
 
 filename = "add_item.json"
 json_list = []
 
+"""str json"""
 if os.path.exists(filename):
     json_list = load_from_json_file(filename)
-    """coment"""
+    """def"""
 
 for index in argv[1:]:
+    """ argv """
     json_list.append(index)
-    """coment"""
+    """append"""
 
 save_to_json_file(json_list, filename)
