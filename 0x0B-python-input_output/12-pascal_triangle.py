07bf176a7803869f7934d666e5360dd3ddea8462
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-10 17:19:05 -0500

    tasks 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""class pascal`s triangle"""
+""" pascal triange """
 
 
 def pascal_triangle(n):
-    fila = [1]
-    cero = [0]
+    """return a pascal triangle"""
+    pasc_list = []
 
     for i in range(n):
-        """print"""
-        n = [i + d for i, d in zip(fila + cero, cero + fila)]
-        
+        pasc_list.append([])
+        pasc_list[i].append(1)
+        for j in range(1, i):
+            pasc_list[i].append(pasc_list[i - 1][j - 1] + pasc_list[i - 1][j])
+            """conditional"""
+        if (n != 0):
+            pasc_list[i].append(1)
+    return pasc_list
