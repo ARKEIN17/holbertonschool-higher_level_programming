a60e27a87608b1bebb189269d4e9434d9cdb4140
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-14 17:59:35 -0500

    tkas 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,8 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Inherits from Base
-    """
+    """class rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
