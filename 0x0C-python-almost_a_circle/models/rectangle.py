447107340e0ab400ad1964c00c35fbea31bd4b60
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-14 19:37:37 -0500

    taks 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
-"""class rectangle
-"""
+'''Module for Rectangle class.'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    """class rectangle
-    """
+    '''A Rectangle class.'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """constructor"""
+        '''Constructor.'''
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-
-    @property
-    def width(self):
-        '''Width of this rectangle.'''
-        return self.__width
-    @width.setter
-    def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
