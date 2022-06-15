fbc794fa61bd093b251710ad395cb72f1800521c
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-14 19:43:15 -0500

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Module for Rectangle class
+    Class Rectangle that inherits from Base
 """
 from models.base import Base
 
@@ -16,3 +16,110 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """Getter method of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method of width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method of height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter method of x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter method of x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter method of y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter method of y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Method to return the area"""
+        return self.__height * self.__width
+
+    def display(self):
+        """Method to print the rectangle"""
+        print("\n" * self.__y, end="")
+        for i in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def update(self, *args, **kwargs):
+        """Method to update every single value"""
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
+        if args is None or len(args) == 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "width":
+                    self.__width = value
+                if key == "height":
+                    self.__height = value
+                if key == "x":
+                    self.__x = value
+                if key == "y":
+                    self.__y = value
+
+    def to_dictionary(self):
+        """Method to return a dictionary"""
+        mydict = {
+            "x": self.x,
+            "y": self.y,
+            "id": self.id,
+            "height": self.height,
+            "width": self.width
+            }
+        return mydict
+
+    def __str__(self):
+        """String representation of the class"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} -\
+ {self.__width}/{self.__height}"
