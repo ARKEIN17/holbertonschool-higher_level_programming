1f20a2803dd8c7b2d4d3b57fbaf3ce7d92a9c775
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-18 22:32:00 -0500

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,3 +16,19 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Method to return a json representation"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """My Funtion to returns python representation of json"""
+        if json_string is None:
+            return []
+        represent = json.loads(json_string)
+        return represent
