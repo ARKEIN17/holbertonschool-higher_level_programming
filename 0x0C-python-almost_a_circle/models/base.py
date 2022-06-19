d5969edc95c7e527571367a83c86a24b35800fa8
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-18 22:51:35 -0500

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python3
-"""base
-"""
+""" Module Base """
 import json
+from os import path
 import csv
 
 
 class Base:
-    """class base
-    """
+    """ class Base """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ Object Builder  """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Method to return a json representation"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
+        """ Returns json str """
+        if list_dictionaries is None:
+            list_dictionaries = []
         return json.dumps(list_dictionaries)
 
-    @staticmethod
-    def from_json_string(json_string):
-        """My Funtion to returns python representation of json"""
-        if json_string is None:
-            return []
-        represent = json.loads(json_string)
-        return represent
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Saves to a file """
+        obj_list = []
+        name = str(cls.__name__) + ".json"
+        with open(name, 'w', encoding='utf8') as file:
+            if list_objs is None:
+                file.write(cls.to_json_string(obj_list))
+            else:
+                for obj in list_objs:
+                    obj_list.append(cls.to_dictionary(obj))
+                file.write(cls.to_json_string(obj_list))
