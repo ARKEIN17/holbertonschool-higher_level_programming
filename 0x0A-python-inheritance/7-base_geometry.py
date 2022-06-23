0d8f6cd343a4ced76a3f9d2cc8e912cca11be48e
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-22 21:13:28 -0500

    test

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-"""Creates a new class"""
+""" module with class BaseGeometry """
 
 
-class BaseGeometry():
-    """New class with area method and integer validator"""
+class BaseGeometry:
+    """BaseGeometry class"""
 
+    def area(self):
+        """method for calculated area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method for validate if a num is integer"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
     def area(self):
         raise Exception("area() is not implemented")
 
