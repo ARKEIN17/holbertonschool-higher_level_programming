e462c1b1bee6ba17e2021224c61b4ed83532b403
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-06-22 21:20:15 -0500

    taks 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""
-Contains the class MyInt
-"""
-
-
+"""Import the class"""
 Rectangle = __import__('9-rectangle').Rectangle
-""" module with class BaseGeometry """
+"""
+    Class Rectangle that inherits from BaseGeometry
+"""
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle that inherits BaseGeometry"""
+    """
+        My class
+    """
 
     def __init__(self, size):
-        """Method for initialized the attrubutes"""
-
-        super().__init__(size, size)
-        self.integer_validator("size", size)
+        """Constructor method"""
+        super().integer_validator("size", size)
         self.__size = size
 
     def area(self):
-        """rectangle area"""
-
-        return self.__size ** 2
+        return self.__size * self.__size
 
     def __str__(self):
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        return f"[Square] {self.__size}/{self.__size}"
