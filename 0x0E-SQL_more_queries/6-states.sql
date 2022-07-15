7c35dc54e7cacbe7192fc7d68491bef8af3550f5
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-07-14 21:29:20 -0500

    taks 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- creates the database hbtn_0d_usa and the table btn_0d_usa.states
+-- VARCHAR(256)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE, name VARCHAR(256) NOT NULL);
