ae96d3f78dd378d603bf984de654fcb07e1e7b39
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-08-16 20:43:58 -0500

    taks 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,10 +2,10 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-    for (const element of list) {
-      if (element === searchElement) {
-        count++;
-          }
-     }
+  for (const element of list) {
+    if (element === searchElement) {
+      count++;
+    }
+  }
   return count;
 };
