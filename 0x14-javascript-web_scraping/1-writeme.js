d0a54c1ae41fc44e675e2f6491250d6b852b7bc1
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-08-17 20:13:54 -0500

    taks 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const { argv } = require('process');
+
+if (argv[3]) {
+  const data = argv[3];
+  fs.writeFile(argv[2], data, (err) => {
+    if (err) { console.log(err); }
+  });
+}
