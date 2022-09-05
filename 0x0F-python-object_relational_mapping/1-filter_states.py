482445cbe7f56577809bd0ca931789566cea30de
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-09-05 18:53:41 -0500

    taks 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """List Module"""
-
-from sqlite3 import Row
 import MySQLdb
 from sys import argv
 
@@ -13,8 +11,5 @@ if __name__ == "__main__":
     mycursor.execute("SELECT * FROM states \
                         WHERE name LIKE BINARY 'N%' ORDER BY id;")
     result = mycursor.fetchall()
-    for result in result:
-        if result[1][0] == "N":
-            print(result)
-    mycursor.close()
-    database.close()
+    for value in result:
+        print(value)
