a9a6e2fcb4e023fe6e9e48f89b7b03303767915e
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-09-05 18:23:36 -0500

    taks 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 """List Module"""
+
+from sqlite3 import Row
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                                    passwd=argv[2], db=argv[3])
+
+    mycursor = database.cursor()
+    mycursor.execute("SELECT * FROM states ORDER BY id;")
+    result = mycursor.fetchall()
+    for result in result:
+            if result[1][0] == "N":
+                    print (result)
+    mycursor.close()
+    database.close()
