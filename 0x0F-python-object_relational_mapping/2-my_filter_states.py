d66b292048f49dedee7b36c4c53c83ce30cd7cba
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-09-05 19:59:54 -0500

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""List Module"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    database = MySQLdb.connect('localhost', argv[1], argv[2], argv[3])
+
+    mycursor = database.cursor()
+    mycursor.execute("SELECT * FROM states \
+                        WHERE name LIKE BINARY 'N%' ORDER BY id;")
+    result = mycursor.fetchall()
+    for result in result:
+        if result[1] == argv[4]:
+            print(result)
+    mycursor.close()
+    database.close()
