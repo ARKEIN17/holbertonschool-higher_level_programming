4d7702ae174918f6593cc61b9ef17d84c901c143
ARKEIN17 <ramirez1kevin98@gmail.com>
2022-09-06 00:17:18 -0500

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""prints the State"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    database = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    sesion = Session(database)
+    sesion.query(State).filter(State.id == 2).update(
+        {State.name: 'New Mexico'}, synchronize_session=False)
+    sesion.commit()
